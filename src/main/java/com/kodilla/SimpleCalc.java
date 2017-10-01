f410d8695eb56043f9ebc85c85cf45f15c10d681
“Nigdziebadz <slowaniepowazne@gmail.com>
2017-10-01 18:35:54 +0200

    Wlasciwy kalulator

--- src/main/java/com/kodilla/SimpleCalc.java
+++ src/main/java/com/kodilla/SimpleCalc.java
@@ -1,4 +1,28 @@
-package com.kodilla;
+class Calc {
 
-public class SimpleCalc {
+    int a;
+    int b;
+
+    public int CalcAplusB(int a, int b) {
+        return a + b;
+    }
+
+    public int CalcAminusB(int a, int b) {
+        return a - b;
+    }
 }
+
+class Apllication
+{
+    public static void main(String[] args) {
+
+        Calc run = new Calc();
+
+        int plusResult = run.CalcAplusB(10, 5);
+        int minusResult = run.CalcAminusB(10, 5);
+
+        System.out.println("a plus b result: " + plusResult);
+        System.out.println("a minus b result: " + minusResult);
+    }
+}
+
