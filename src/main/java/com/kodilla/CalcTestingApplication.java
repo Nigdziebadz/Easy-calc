3f16e936a00797ae69ddc8aca970f9cde3c4ddf7
“Nigdziebadz <slowaniepowazne@gmail.com>
2017-10-01 11:52:39 +0200

    East Calc

---
+++ src/main/java/com/kodilla/CalcTestingApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcTestingApplication.class, args);
+	}
+}
---
+++ src/main/java/eastCalc.java
@@ -0,0 +1,28 @@
+class Calc {
+
+    int a;
+    int b;
+
+    public int CalcAplusB(int a, int b) {
+        return a + b;
+    }
+
+    public int CalcAminusB(int a, int b) {
+        return a - b;
+    }
+}
+
+class Apllication
+{
+    public static void main(String[] args) {
+
+        Calc run = new Calc();
+
+        int plusResult = run.CalcAplusB(10, 5);
+        int minusResult = run.CalcAminusB(10, 5);
+
+        System.out.println(plusResult);
+        System.out.println(minusResult);
+    }
+}
+
